829e9af6c064e43d9cfa6a6b5537ed6a177f6a17
Juan Figuera <juan_figuera@hotmail.com>
2013-04-21 18:43:06 -0400

    add content pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 GeeksAndSuits::Application.routes.draw do
+  get "content/silver"
+
+  get "content/gold"
+
+  get "content/platinum"
+
   authenticated :user do
     root :to => 'home#index'
   end

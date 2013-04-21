21f90baf99c2050eea4a7987c20beea6ea25c78c
Juan Figuera <juan_figuera@hotmail.com>
2013-04-20 22:48:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeeksAndSuits::Application.config.secret_token = 'ba63439d6e76538efbed1c06fc77fc958887c347f45289dc4d6fb3e179dcee1652d1e81b202dd364974f4ae0ac0f97b18480b21b136bedf497ef64aed2835b9a'

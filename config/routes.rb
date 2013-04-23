04293a525ca9191a13dd52e2a7f52d661c6e244f
Juan Figuera <juan_figuera@hotmail.com>
2013-04-22 21:58:31 -0400

    add Stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
-RailsStripeMembershipSaas::Application.routes.draw do
+GeeksAndSuits::Application.routes.draw do
+  mount StripeEvent::Engine => '/stripe'
   get "content/gold"
   get "content/silver"
   get "content/platinum"
-  
   authenticated :user do
     root :to => 'home#index'
   end
-  
   root :to => "home#index"
-  
   devise_for :users, :controllers => { :registrations => 'registrations' }
   devise_scope :user do
     put 'update_plan', :to => 'registrations#update_plan'
